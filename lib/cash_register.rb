f296df00b0cf20d347308c6addfb8fdd7c2f4f9c
Arthur Song <arth3rs0ng@gmail.com>
2019-04-07 05:08:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,14 +10,13 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     @total += (price * quantity)
-    if quantity == 1
+
+    counter = 0
+    until counter == quantity
       @items << title
-    else
-      counter = 0
-      until counter == quantity
-        
-      end
+      counter += 1
     end
+
   end
   
   def apply_discount
