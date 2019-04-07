5da7186478df34f23175656adf40fa9867621ffb
Arthur Song <arth3rs0ng@gmail.com>
2019-04-07 05:02:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,5 +15,6 @@ class CashRegister
     @total = @total.to_f
     percentage = (100.0 - @discount.to_f) / 100.0
     @total = @total * percentage
+    "After the discount, the total comes to $#{@total}."
   end
 end
